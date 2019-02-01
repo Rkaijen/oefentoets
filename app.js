f4b86618fa376b2e98ccbd096bd07c42c45ec821
rkaijen <rob.kayen@gmail.com>
2019-02-01 15:26:14 +0100

    moved js folder, assets folder removed.

--- app.js
+++ app.js
@@ -1,2 +1,2 @@
-const { Boat, Tour, Price } = require("./assets/js/classes");
-const { tour_arr } = require("./assets/js/data");
+const { Boat, Tour, Price } = require("./js/classes");
+const { tour_arr } = require("./js/data");
